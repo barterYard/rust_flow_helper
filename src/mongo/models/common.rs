cc73b0f733dd1ba0563c9b97fc51084019443da1
Alexandre <alexandr.barbier@gmail.com>
2023-10-26 16:43:33 +0200

    add projection support for ModelCollection

--- src/mongo/models/common.rs
+++ src/mongo/models/common.rs
@@ -11,4 +11,9 @@ where
             .database(Self::get_db_name().as_str())
             .collection::<Self>(Self::get_col_name().as_str())
     }
+    fn get_collection_document(client: &Client) -> mongodb::Collection<Document> {
+        client
+            .database(Self::get_db_name().as_str())
+            .collection::<Document>(Self::get_col_name().as_str())
+    }
 }
